1f74a91b3d782b52872e64852d87ed80696595a9
Joy Narayon Roy <68392629+joyroy-bd@users.noreply.github.com>
2021-03-18 18:21:49 +0600

    Delete pdf.py

--- pdf.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2 
-
-
-pdfFile = open('bpl.pdf','rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFile)
-#print(dir(pdfReader))
-page = pdfReader.numPages
-text = pdfReader.getPage(0).extractText()
-print(text)
